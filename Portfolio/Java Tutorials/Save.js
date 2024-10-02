7e417f6078db75d7206fff368179f609f681fbe4
Andigema Joyce Ani <164731266+Joyce-Ani@users.noreply.github.com>
2024-10-02 08:27:41 +0100

    Add files via upload

---
+++ Portfolio/Java Tutorials/Save.js
@@ -0,0 +1,9 @@
+//Creating Element
+const ul = document.querySelector('ul');
+const li=document.createElement('li');
+
+//Adding Element
+ul.append(li)
+
+//Modifying Elemnt
+ li.innerText ='Hi there';
